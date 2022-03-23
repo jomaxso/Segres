c6ec0140fd63517b21a5498d67a73b4047e2bb6c
jomaxsorge <jomaxsorge@outlook.de>
2022-03-23 16:32:16 +0100

    test commit

---
+++ src/MicrolisR/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MicrolisR
+{
+    internal class Class1
+    {
+    }
+}
