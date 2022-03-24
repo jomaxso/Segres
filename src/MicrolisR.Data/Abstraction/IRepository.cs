647c017fbe1d0076a9e2fd5612442d75fd6fe3ae
jomaxsorge <jomaxsorge@outlook.de>
2022-03-24 21:20:10 +0100

    Changed Data Repository
    Rebuild sourceGenerator  --> old version ist not working

--- src/MicrolisR.Data/Abstraction/IRepository.cs
+++ src/MicrolisR.Data/Abstraction/IRepository.cs
@@ -2,6 +2,7 @@
 
 public interface IRepository<TId, TEntity>
     where TEntity : IEntity<TId>
+    where TId : struct
 {
     Task<IEnumerable<TEntity>> GetAsync();
     Task<TEntity?> GetAsync(TId id);
