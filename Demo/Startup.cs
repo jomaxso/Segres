cd151844d35af01b56d5af5292471c4189c9d90c
jomaxsorge <jomaxsorge@outlook.de>
2022-07-27 11:27:57 +0200

    init Endpoints

--- Demo/Startup.cs
+++ Demo/Startup.cs
@@ -1,60 +1,74 @@
 ﻿using System.Reflection;
-using Demo.Endpoints.PrintToConsole;
 using MicrolisR;
+using PrintToConsole;
 
 namespace Demo;
 
-public static class Startup
+public static class DependencyInjection
 {
-    private static void MapMicrolisR(this IEndpointRouteBuilder app)
-    {
-        // app.MapPost("/", (IMediator mediator, [FromBody] PrintCommand command, CancellationToken cancellationToken) => mediator.SendAsync(command, cancellationToken));
-        //
-        // app.MapPost("/{value:int}", ([FromServices] IMediator mediator, [FromRoute] int value, [FromHeader] int value2, CancellationToken cancellationToken) => mediator.SendAsync(new PrintCommand {Value = value, Value2 = value2}, cancellationToken));
-        //
-        // app.MapPost("/", (IMediator mediator, [FromQuery] int value, CancellationToken cancellationToken) => mediator.SendAsync(new PrintCommand {Value = value}, cancellationToken));
-        //
-        // app.MapPost("/", (IMediator mediator, [FromHeader] int value, CancellationToken cancellationToken) => mediator.SendAsync(new PrintCommand {Value = value}, cancellationToken));
-        //
-
-
-        // app.MapGet("/", async (IMediator mediator, [FromQuery] int value, CancellationToken cancellationToken) =>
-        // {
-        //     var request = new PrintCommand
-        //     {
-        //         Value = value
-        //     };
-        //     
-        //     return await mediator.SendAsync(request, cancellationToken);
-        // });
-        //
-        // app.MapGet("/{value:int}", async (IMediator mediator, [FromRoute] int value, CancellationToken cancellationToken) => await mediator.SendAsync(new PrintCommand {Value = value}, cancellationToken));
-        //
-        // app.MapGet("/", async (IMediator mediator, [FromHeader] int value, CancellationToken cancellationToken) => await mediator.SendAsync(new PrintCommand {Value = value}, cancellationToken));
-    }
-
     public static IServiceCollection AddEndpoints(this IServiceCollection services)
     {
         var assemblies = new List<Assembly> {Assembly.GetCallingAssembly()};
 
         var entryAssembly = Assembly.GetEntryAssembly();
-        
+
         if (entryAssembly is not null)
             assemblies.Add(entryAssembly);
 
         var interfaceType = typeof(IHttpContextRequestResolver);
-        
+
         foreach (var assembly in assemblies.Distinct())
         {
             var endpointResolvers = assembly.GetClassesImplementingInterface(interfaceType);
 
             foreach (var endpointResolver in endpointResolvers)
             {
-                services.Add(new ServiceDescriptor(interfaceType,endpointResolver, ServiceLifetime.Singleton));
+                services.Add(new ServiceDescriptor(interfaceType, endpointResolver, ServiceLifetime.Singleton));
             }
         }
-        
+
         return services;
+    }
+
+    public static void MapEndpoints(this IEndpointRouteBuilder app)
+    {
+        var httpContextRequestResolvers = app.ServiceProvider
+            .GetServices<IHttpContextRequestResolver>()
+            .DistinctBy(x => x.GetType());
+
+        foreach (var httpContextRequestResolver in httpContextRequestResolvers)
+            httpContextRequestResolver.Initialize(app);
+    }
+    
+    public static void MapEndpoint<TRequest, TResponse>(this IEndpointRouteBuilder app, Http verb, string pattern)
+        where TRequest : IRequestable<TResponse>
+    {
+        string[] verbs = {verb.ToString()};
+
+        var mediator = app.ServiceProvider.GetRequiredService<IMediator>();
+        var httpContextRequestResolver = app.ServiceProvider.GetRequiredService<IHttpContextRequestResolver>();
+
+        app.MapMethods(pattern, verbs, async context =>
+        {
+            var request = await httpContextRequestResolver.ExtractRequest<TRequest>(context.Request, context.RequestAborted);
+
+            mediator.Validate(request);
+
+            if (request is IRequestable withoutResponse)
+            {
+                await mediator.SendAsync(withoutResponse, context.RequestAborted);
+                await context.Response.CompleteAsync();
+                return;
+            }
+
+            var response = await mediator.SendAsync(request, context.RequestAborted);
+
+            if (response is IValidatable validatable)
+                mediator.Validate(validatable);
+
+            await context.Response.WriteAsJsonAsync(response);
+            await context.Response.CompleteAsync();
+        });
     }
 
     private static IEnumerable<Type> GetClassesImplementingInterface(this Assembly assembly, Type typeToMatch)
@@ -68,73 +82,40 @@ public static class Startup
             return !type.IsInterface && !type.IsAbstract && isImplementRequestType;
         }).ToList();
     }
-    
-    public static void MapEndpoints(this IEndpointRouteBuilder app)
-    {
-        var httpContextRequestResolvers = app.ServiceProvider
-            .GetServices<IHttpContextRequestResolver>()
-            .DistinctBy(x => x.GetType());
-        
-        foreach (var httpContextRequestResolver in httpContextRequestResolvers)
-            httpContextRequestResolver.MapEndpoints(app);
-    }
-    
-    public static void MapEndpoint<TRequest, TResponse>(this IEndpointRouteBuilder app, Http verb, string pattern, IRequestHandler<TRequest, TResponse>? _)
-        where TRequest : IRequestable<TResponse>
-    {
-        string[] verbs = { verb.ToString() };
-        
-        var mediator = app.ServiceProvider.GetRequiredService<IMediator>();
-        var httpContextRequestResolver = app.ServiceProvider.GetRequiredService<IHttpContextRequestResolver>();
-
-        app.MapMethods(pattern, verbs, async context =>
-        {
-            var request = await httpContextRequestResolver.ExtractRequest<TRequest>(context.Request, context.RequestAborted);
-            
-            mediator.Validate(request);
-
-            if (request is IRequestable withoutResponse)
-            {
-                await mediator.SendAsync(withoutResponse, context.RequestAborted);
-                await context.Response.CompleteAsync();
-                return;
-            }
-            
-            var response = await mediator.SendAsync(request, context.RequestAborted);
-            
-            if (response is IValidatable validatable)
-                mediator.Validate(validatable);
-
-            await context.Response.WriteAsJsonAsync(response);
-            await context.Response.CompleteAsync();
-            
-           
-        });
-    }
 }
 
 public interface IHttpContextRequestResolver
 {
-    void MapEndpoints(IEndpointRouteBuilder app);
+    internal void Initialize(IEndpointRouteBuilder app);
 
     Task<TRequest> ExtractRequest<TRequest>(HttpRequest httpRequest, CancellationToken cancellationToken);
+}
+
+public interface IHttpContextRequestResolver<TRequest, TResponse> : IHttpContextRequestResolver
+    where TRequest : IRequestable<TResponse>
+{
+    void IHttpContextRequestResolver.Initialize(IEndpointRouteBuilder app)
+    {
+        var handler = app.ServiceProvider.GetRequiredService(typeof(IRequestHandler<TRequest, TResponse>)); // BUG not found in DI
+        
+        // TODO REfelction get all Attribute infos
+
+        var verb = Http.GET;
+        var pattern = "/";
+        
+        app.MapEndpoint<TRequest, TResponse>(verb, pattern);
+    }
 }
 
 
 /// <summary>
 /// TO GENERATE
 /// </summary>
-
-public class HttpContextRequestResolver : IHttpContextRequestResolver //  Generate Source
+public class HttpContextRequestResolver : IHttpContextRequestResolver<PrintRequest, Unit> //  Generate Source
 {
-    public void MapEndpoints(IEndpointRouteBuilder app)
-    {
-        app.MapEndpoint(Http.GET, "/", app.ServiceProvider.GetService<PrintEndpoint>());
-    }
-    
     public async Task<TRequest> ExtractRequest<TRequest>(HttpRequest httpRequest, CancellationToken cancellationToken)
     {
-         var request =  await httpRequest.ReadFromJsonAsync<TRequest>(cancellationToken);
+        var request = await httpRequest.ReadFromJsonAsync<TRequest>(cancellationToken);
 
         if (request is null)
             throw new Exception();
@@ -142,4 +123,3 @@ public class HttpContextRequestResolver : IHttpContextRequestResolver //  Genera
         return request;
     }
 }
-
